87e5a0cb6419fb172d4f0a384a5c761d55878b74
ponchodeaf <22062377+PonchoBT@users.noreply.github.com>
2021-08-22 01:00:35 -0500

    Values and Variables

---
+++ valuesandvariables/script.js
@@ -0,0 +1,14 @@
+// let firstName = "Alfonso";
+// console.log(firstName);
+
+
+// let firstName = "Alfonso";
+// let lastName = "Balderas";
+// console.log(firstName, lastName);
+
+
+// let first = "2";
+// let last = "2";
+// console.log(first + last);
+
+
