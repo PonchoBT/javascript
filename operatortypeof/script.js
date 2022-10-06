b124a76ed2cae96fe5d11da7b63688ed94b9bc88
Alfonso BT <alfonsobalderastorres@gmail.com>
2022-10-06 00:25:34 -0500

    New operatiors

--- operatortypeof/script.js
+++ operatortypeof/script.js
@@ -1,1 +1,16 @@
-let me = {name: 'alfonso' };
+console.log(30+40);
+console.log(30-40);
+console.log(30*40);
+console.log(30/40);
+
+const addition = 100 + 100;
+console.log(addition);
+
+const subteraction = 100 - 100;
+console.log(subteraction);
+
+const multiplication = 100 * 100;
+console.log(multiplication);
+
+const division = 100 / 100;
+console.log(division);
