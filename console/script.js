03b86882b03c062192b2634425826c55ce906588
Alfonso BT <ponchobt.dev@gmail.com>
2023-06-12 20:59:34 -0600

    Const saludo

--- console/script.js
+++ console/script.js
@@ -1,12 +1,23 @@
+// let a = 40, 
+//     b = 50,
+//     c = 10,
+//     d = 10,
+//     x = a + b;
+    
+// console.log("Suma:", b + d);
+// console.log(x);
+// console.warn("warn");
+// console.error("error");
+// console.log({a});
+// console.log("%c Hola", "color:blue; font-weight:bold");
+
+
 let a = 40, 
     b = 50,
-    c = 10,
-    d = 10,
+    c = "Hola ",
+    d = "Alfonso",
     x = a + b;
-    
-console.log("Suma:", b + d);
-console.log(x);
-console.warn("warn");
-console.error("error");
-console.log({a});
-console.log("%c Hola", "color:blue; font-weight:bold");
+
+const saludo = c + d;
+
+// console.table({a, b, c, d, x})
